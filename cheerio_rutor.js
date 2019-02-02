a4d9d93a75ab1396b94194da9c56c458fdbaff9f
it4csl <it4csl@gmail.com>
2019-02-02 23:06:24 +0200

    добавление new, разделение данных на 2 массива

--- cheerio_rutor.js
+++ cheerio_rutor.js
@@ -6,35 +6,42 @@ const request = require("request");
 
 let app = express();
 
-let url = "http://rutor.info/new";
+let url_new = "http://rutor.info/new";
+let url_top = "http://rutor.info/top";
 
-let arrr = [];
+let arr_top = [];
+let arr_new = [];
 
-request(url, (error, response, body) => {
-  if(!error) {
-    $ = cheerio.load(body);
-    let name1 = $(".tum a");
-    let name2 = $(".gai a");
-    let parce = (arg) => {
-      for (let i = 0; i < $(arg).length; i++) {
-        var text = $(arg[i]).text();
-        if(text.length === 0) continue;
-        var found = false;
-        if( text.indexOf("iTunes") !== -1 ) {
-          found = true;
-        }
-        if( text.indexOf("Лицензия") !== -1 ) {
-          found = true;
-        }
-        if (found) {
-          arrr.push(`<tr><td><a href="http://rutor.info/${$(arg[i]).attr("href")}">${text}</a></td></tr>`);
+let func = (ur, arr) => {
+  request(ur, (error, response, body) => {
+    if(!error) {
+      $ = cheerio.load(body);
+      let name1 = $(".tum a");
+      let name2 = $(".gai a");
+      let parce = (arg) => {
+        for (let i = 0; i < $(arg).length; i++) {
+          var text = $(arg[i]).text();
+          if(text.length === 0) continue;
+          var found = false;
+          if( text.indexOf("iTunes") !== -1 ) {
+            found = true;
+          }
+          if( text.indexOf("Лицензия") !== -1 ) {
+            found = true;
+          }
+          if (found) {
+            arr.push(`<tr><td><a href="http://rutor.info/${$(arg[i]).attr("href")}">${text}</a></td></tr>`);
+          }
         }
       }
+      parce(name1);
+      parce(name2);
     }
-    parce(name1);
-    parce(name2);
-  }
-});
+  });
+}
+
+func(url_top, arr_top);
+func(url_new, arr_new);
 
 app.get("/", (req, res) => {
   res.end(`
@@ -42,24 +49,22 @@ app.get("/", (req, res) => {
         <head>
           <meta charset="utf-8">
           <style>
-            body, table{
-              font-family: Tahoma, Verdana, Arial, Helvetica, sans-serif;
-              font-size: 12px;
-            }
-            a{
-              text-decoration: none;
-              color: #000000;
-            }
-            tr:hover {
-              background: #bccae0; 
-              color: #ffe; 
-            } 
+            body, table{ font-family: Tahoma, Verdana, Arial, Helvetica, sans-serif; font-size: 12px; }
+            a{ text-decoration: none; color: #000000; }
+            tr:hover { background: #bccae0; color: #ffe; } 
           </style>
         </head>
         <body>
           <table>
             <tbody>
-              ${arrr.join('')}
+              <h3>Top</h3>
+              ${arr_top.join('')}
+            </tbody>
+          </table>
+          <table>
+            <tbody>
+              <h3>New</h3>
+              ${arr_new.join('')}
             </tbody>
           </table>
         </body>
