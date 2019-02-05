41c42eb46c92575a36a1958965d4b6b60078f8b2
it4csl <it4csl@gmail.com>
2019-02-05 19:03:20 +0200

    разделение данных, сохранение в хеш

--- test_rutor.js
+++ test_rutor.js
@@ -6,27 +6,32 @@ const request = require("request");
 
 const app = express();
 
-const url_new = "http://rutor.info/new", url_top = "http://rutor.info/top";
+const url_new = "http://rutor.info/new", url_top = "http://rutor.info/browse/0/1/0/2";
 
 const arr_top = [], arr_new = [];
+const arr_obj = {};
 
 request(url_top, (error, response, body) => {
   if(!error) {
     $ = cheerio.load(body);
-    const name1 = $(".tum"), name2 = $(".gai a");
+    const tum = $(".tum"), gai = $(".gai a");
+
+    get_data = (from_parse) => {
+      for(let i = 0; i < from_parse.length; i++) {
+        let url = "http://rutor.info" + $($(from_parse[i]).children("td").children("a")[2]).attr("href");
+        let text = $($(from_parse[i]).children("td").children("a")[2]).text();
+        let size = $($(from_parse[i]).children("td")[3]).text();
+
+        arr_obj[i] = {url, text, size};
+      }
+    }
+    get_data(tum);
     
-    let href_torrent = "http://rutor.info" + $($(name1[1]).children("td").children("a")[2]).attr("href");
-    let text = $($(name1[1]).children("td").children("a")[2]).text();
-    let size = $($(name1[1]).children("td")[3]).text();
+    //let out_html = `<tr><td><a href="${href_torrent}">${text}</a></td><td>${size}</td></tr>`; 
 
-    let out_html = `<tr><td><a href="${href_torrent}">${text}</a></td><td>${size}</td></tr>`;
-    arr_top.push(out_html);
-
-    console.log(text);
-    console.log(href_torrent);
-    console.log(size);
-    console.log("");
-    console.log(out_html);
-
+  }
+  for(let i = 0; i < Object.keys(arr_obj).length; i++) {
+    let out = `${arr_obj[i].url} || ${arr_obj[i].text} || ${arr_obj[i].size}`;
+    console.log(out);
   }
 });
