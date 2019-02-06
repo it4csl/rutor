ff0f5747525e865aab74a0631f37d8a89f858a33
it4csl <it4csl@gmail.com>
2019-02-06 13:55:22 +0200

    решен вопрос с парсингом размера раздачь без комментариев и сортировкой

--- test_rutor.js
+++ test_rutor.js
@@ -6,9 +6,9 @@ const request = require("request");
 
 const app = express();
 
-const url_new = "http://rutor.info/new", url_top = "http://rutor.info/browse/0/1/0/2";
+url_z_kino = "http://rutor.info/browse/0/1/0/2";
 
-const arr_top = [], arr_new = [];
+const arr_obj = [];
 
 out_obj = (arr) => {
   for(let i = 0; i < arr.length; i++) {
@@ -17,37 +17,35 @@ out_obj = (arr) => {
   }
 }
 
-request(url_top, (error, response, body) => {
+request(url_z_kino, (error, response, body) => {
   if(!error) {
     $ = cheerio.load(body);
-    const tum = $("#index").children("table").children("tbody").children("tr")
+    const obj_list_tr = $("#index").children("table").children("tbody").children("tr")
 
     get_data = (from_parse, in_obj) => {
+      let size = 0;
       for(let i = 1; i < from_parse.length; i++) {
         let url = "http://rutor.info" + $($(from_parse[i]).children("td").children("a")[2]).attr("href");
         let text = $($(from_parse[i]).children("td").children("a")[2]).text();
-        let size = $($($(from_parse[i]).children("td")[3]).attr("align", "right")).text();
+ 
+        if(Object.keys($($($(from_parse[i]).children("td")))).length === 8) {
+          size = $($($(from_parse[i]).children("td")[2])).text();
+        }
+        if(Object.keys($($($(from_parse[i]).children("td")))).length === 9) {
+          size = $($($(from_parse[i]).children("td")[3])).text();
+        }
 
         in_obj.push({text, url, size});
       }
     }
-    //get_data(tum, arr_top);
-    let size = $($($(tum[61]).children("td"))).text();  // разобраться с размером когда -1 td (раздача без комментариев)
-
-    console.log(size)
-    
+    get_data(obj_list_tr, arr_obj);   
   }
 
-  arr_top.sort((a, b) => {
-    // let nameA=a.size, nameB=b.size;
-    // if (nameA < nameB) return -1;
-    // if (nameA > nameB) return 1;
-    return a.size - b.size;
+  arr_obj.sort((a, b) => {
+    let nameA=a.text, nameB=b.text;
+    if (nameA < nameB) return -1;
+    if (nameA > nameB) return 1;
+    return 0;
   });
-
-  
-  
-  // out_obj(obj_gai);
-  //console.log(arr_top[0].text);
 });
-//setTimeout(() => out_obj(arr_top), 2000);
+setTimeout(() => out_obj(arr_obj), 2000);
