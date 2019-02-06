e21b55975446c3c4192aa6b9cd653caed908d338
it4csl <it4csl@gmail.com>
2019-02-06 02:58:19 +0200

    смена сохранения данных

--- test_rutor.js
+++ test_rutor.js
@@ -9,32 +9,45 @@ const app = express();
 const url_new = "http://rutor.info/new", url_top = "http://rutor.info/browse/0/1/0/2";
 
 const arr_top = [], arr_new = [];
-const obj_tum = {}, obj_gai = {};
+
+out_obj = (arr) => {
+  for(let i = 0; i < arr.length; i++) {
+    let out = `${arr[i].text} || ${arr[i].size}`;
+    console.log(i + ") " + out);
+  }
+}
 
 request(url_top, (error, response, body) => {
   if(!error) {
     $ = cheerio.load(body);
-    const tum = $(".tum"), gai = $(".gai");
+    const tum = $("#index").children("table").children("tbody").children("tr")
 
     get_data = (from_parse, in_obj) => {
-      for(let i = 0; i < from_parse.length; i++) {
+      for(let i = 1; i < from_parse.length; i++) {
         let url = "http://rutor.info" + $($(from_parse[i]).children("td").children("a")[2]).attr("href");
         let text = $($(from_parse[i]).children("td").children("a")[2]).text();
-        let size = $($(from_parse[i]).children("td")[3]).text();
+        let size = $($($(from_parse[i]).children("td")[3]).attr("align", "right")).text();
 
-        in_obj[i] = {url, text, size};
+        in_obj.push({text, url, size});
       }
     }
-    get_data(tum, obj_tum);
-    //get_data(gai, obj_gai);
+    //get_data(tum, arr_top);
+    let size = $($($(tum[61]).children("td"))).text();  // разобраться с размером когда -1 td
+
+    console.log(size)
+    
   }
-  // out_obj = (obj) => {
-  //   for(let i = 0; i < Object.keys(obj).length; i++) {
-  //     let out = `${obj[i].url} || ${obj[i].text} || ${obj[i].size}`;
-  //     console.log(out);
-  //   }
-  // }
-  // out_obj(obj_tum);
+
+  arr_top.sort((a, b) => {
+    // let nameA=a.size, nameB=b.size;
+    // if (nameA < nameB) return -1;
+    // if (nameA > nameB) return 1;
+    return a.size - b.size;
+  });
+
+  
+  
   // out_obj(obj_gai);
-  console.log(Object.keys(obj_tum).length);
+  //console.log(arr_top[0].text);
 });
+//setTimeout(() => out_obj(arr_top), 2000);
